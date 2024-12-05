ddd569cf041ee316f8bbe6045da22b78e02b6a36
tiagosantos <132015024+tiagodasbabes23@users.noreply.github.com>
2024-12-05 20:10:21 +0000

    menu alterado

--- controller.py
+++ controller.py
@@ -2,16 +2,16 @@
 def verificar_jogador(matriz,nome):
     
     for j in matriz:
-          if nome == j[0]:
+          if nome == j["Nome"]:
                return True
     return False        
    
    
-def registar_jogador(matriz,nome):
+def registar_jogador(matriz,nome,cor):
     if verificar_jogador(matriz,nome) == True:
           return False
     else:     
-     j = [nome, 0]
+     j = {"Nome": nome, "Pontuação": 0, "Cor": cor}
      matriz.append(j)
      return True
     
--- view.py
+++ view.py
@@ -5,27 +5,41 @@ def main():
     while True:
 
         #------Menu-----
-        operacao = int(input('\t\tMenu\n Escolha uma das seguintes operações:\n1 --> Registar Jogadores \n2--> Jogar \n3-->Pontuações \n4 --> Sair\n-->'))
+        operacao = int(input('\t\tMenu\n Escolha uma das seguintes operações:\n[1] - Registar Jogadores \n[2] - Jogar \n[3] - Pontuações \n[4] - Sair\nQual a sua opção: '))
 
         match operacao:
             case 1:
+                  #---------Registar Jogador-------------------
                     nome = input ('Nome do jogador:\n-->')
-                    verifica = registar_jogador(jogadores, nome)
+                    cor = input('Escolha a cor da sua peça:\n-->')
+                    verifica = registar_jogador(jogadores, nome, cor)
                     if verifica == False:
                          print('Nome de jogador ja existente')
                     else:
                          print('Jogador registado com sucesso\n')
                          print(jogadores)
             case 2:
-                  #--------JOGAR--------
+                  #--------Verificar o número de jogadores antes de jogar-------- 
                  players = int(input('Quantos jogadores vão jogar, escolha entre 2 a 4\n-->'))
-                 if players == 2 or players == 3 or players == 4:
-                      pass
-                 else:
-                      print('O que inseriu está incorreto')
+
+                 n = 0   
+                 while n < players:
+                    nome = input("Qual o nome do jogador: ")
+                    verificar_nome = verificar_jogador(jogadores,nome)
+                    if verificar_nome == False:
+                         print("O utilizador não existe!") 
+                         break    
+
+                    n+=1
+
+                    #------------------------------------------------------------------------------------ 
+
             case 3:
-                  pass
-                         
+                  #---------Pontuações-------
+               for j in jogadores:
+                    print(f"Jogador: {j["Nome"]}, pontuação: {j["Pontuação"]}, cor: {j["Cor"]}")
+
             case 4:
+                  #---------Sair----------
                 print ('Bye Bye, volte sempre Kiss Kiss :)')
                 break
