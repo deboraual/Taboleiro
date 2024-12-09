eb05034e9657ed0841e425b4c22336bbc7c41860
unknown <30015348@students.ual.pt>
2024-12-09 10:55:59 +0000

    taboleiro com numeros em cima e baixo

--- view.py
+++ view.py
@@ -33,10 +33,24 @@ def main():
                          print('\tAVISO: O tabuleiro tem de ter entre 5-15 linhas e 5-15 colunas\n')
                          linhas = int(input('Digite o numero de linhas que deseja que o seu tabuleiro tenha:\n-->'))
                          colunas = int(input('Digite o numero de colunas que quer que o seu tabuleiro tenha:\n-->'))
-                         if linhas >=5 and linhas <=15 and colunas >= 5 and colunas <= 15:
+                         if linhas >=5 and colunas >= 5 and linhas <= 15 and colunas <=15 :
                              tabuleiro = criar_tabuleiro_listas (linhas, colunas)
-                             for linha in tabuleiro:
-                                  print ("\t"+" ".join(linha))
+
+                             #join constroi uma string com o nunmero das colunas alinhando a direita a partir de f"{i+1:>2}", 
+                             #i+1: Os números começam de 1 (as colunas são numeradas a partir de 1), 
+                             #:>2: Garante que cada número ocupe pelo menos 2 caracteres e esteja alinhado à direita,
+                             #for i in range(colunas), coloca os índices de 0 até colunas-1, 
+                             #cada índice é incrementado em 1 (com i+1) para representar o número da coluna.
+                             colunas_numeros ="  "+ "".join(f"{i+1:>2}" for i in range(colunas))
+                             print(colunas_numeros)
+
+                             #idx -> inodice começa do 0 
+                             # f"{idx + 1:<3}" - idx + 1(comeca non 1)
+                             # <3 faz com que ocupe pelo menos 2 caracteres e esteja alinhado a esquerda, 
+                             # (faz com que esteja alinhado com os numeros da lista)
+                             for idx, linha in enumerate(tabuleiro):
+                                print(f"{idx + 1:<3}" + " ".join(linha))  # Número da linha alinhado à esquerda
+
                          else:
                               print ('ERRO: Tamanho de tabuleiro inválido')
 
